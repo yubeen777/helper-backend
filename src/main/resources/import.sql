d1d1ba7ca00cbe3efbaba2eb986918b5fd7da545
yubeen777 <panda7715@naver.com>
2026-05-19 13:56:53 +0900

    fix: import.sql 전환, application.yml defer-datasource-initialization 위치 수정

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO exercises (name, body_part, created_at, updated_at) VALUES
+    ('벤치프레스', 'CHEST', NOW(), NOW()),
+    ('스쿼트', 'LEG', NOW(), NOW()),
+    ('데드리프트', 'BACK', NOW(), NOW()),
+    ('풀업', 'BACK', NOW(), NOW()),
+    ('숄더프레스', 'SHOULDER', NOW(), NOW());
